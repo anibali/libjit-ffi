14b5149ace3e59b579c5dd206a1a6418ab193450
Aiden Nibali <dismaldenizen@gmail.com>
2010-08-15 08:49:22 +1000

    added test for label wrapping

--- Rakefile
+++ Rakefile
@@ -52,7 +52,7 @@ Spec::Rake::SpecTask.new('spec:rcov') do |t|
 end
 
 RCov::VerifyTask.new('spec:rcov:verify' => 'spec:rcov') do |t|
-  t.threshold = 90.41
+  t.threshold = 90.86
   t.index_html = 'coverage/index.html'
 end
 
---
+++ spec/label_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe JIT::Label do
+  describe ".wrap" do
+    it "should correctly wrap label pointers" do
+      in_function do |f|
+        lbl_jit_t = f.label.jit_t
+        JIT::Label.wrap(f, lbl_jit_t).jit_t.address.should eql(lbl_jit_t.address)
+      end
+    end
+  end
+end
+
