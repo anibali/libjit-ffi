484e1b031e19c7c25934cad58f6f6eee72310141
Aiden Nibali <dismaldenizen@gmail.com>
2010-11-19 11:55:30 +1100

    Added more stdio functions

--- lib/libjit/libc.rb
+++ lib/libjit/libc.rb
@@ -6,7 +6,73 @@ module JIT
     FUNCTIONS = {}
     
     [
-      # stdlib
+     
+      #########
+      # stdio #
+      #########
+      
+      # Operations on files
+      [:remove, [:stringz], :int32],
+      [:rename, [:stringz, :stringz], :int32],
+      [:tmpfile, [], :pointer],
+      [:tmpnam, [:stringz], :stringz],
+      
+      # File access functions
+      [:fclose, [:pointer], :int32],
+      [:fflush, [:pointer], :int32],
+      [:fopen, [:stringz, :stringz], :pointer],
+      [:freopen, [:stringz, :stringz, :pointer], :pointer],
+      [:setbuf, [:pointer, :stringz], :void],
+      #[:setvbuf, [:pointer, :stringz, :int32, :size_t], :void],
+      
+      # Formatted input/output functions
+      [:fprintf, [:pointer, :stringz, :varargs], :int32],
+      [:fscanf, [:pointer, :stringz, :varargs], :int32],
+      [:printf, [:stringz, :varargs], :int32],
+      [:scanf, [:stringz, :varargs], :int32],
+      #[:snprintf, [:stringz, :size_t, :stringz, :varargs], :int32],
+      [:sprintf, [:stringz, :stringz, :varargs], :int32],
+      [:sscanf, [:stringz, :stringz, :varargs], :int32],
+      #vprintf
+      #vscanf
+      #vsnprintf
+      #vsprintf
+      #vsscanf
+      
+      #Character input/output functions
+      [:fgetc, [:pointer], :int32],
+      [:fgets, [:stringz, :int32, :pointer], :stringz],
+      [:fputc, [:int32, :pointer], :int32],
+      [:fputs, [:stringz, :pointer], :int32],
+      [:getc, [:pointer], :int32],
+      [:getchar, [], :int32],
+      [:gets, [:stringz], :stringz],
+      [:putc, [:int32, :pointer], :int32],
+      [:putchar, [:int32], :int32],
+      [:puts, [:stringz], :int32],
+      [:ungetc, [:int32, :pointer], :int32],
+      
+      # Direct input/output functions
+      #[:fread, [:pointer, :size_t, :size_t, :pointer], :size_t],
+      #[:fwrite, [:pointer, :size_t, :size_t, :pointer], :size_t],
+      
+      # File positioning functions
+      [:fgetpos, [:pointer, :pointer], :int32],
+      [:fseek, [:pointer, :intn, :int32], :int32],
+      [:fsetpos, [:pointer, :pointer], :int32],
+      [:ftell, [:pointer], :intn],
+      [:rewind, [:pointer], :void],
+      
+      # Error-handling functions
+      [:clearerr, [:pointer], :void],
+      [:feof, [:pointer], :int32],
+      [:ferror, [:pointer], :int32],
+      [:perror, [:stringz], :void],
+      
+      ##########
+      # stdlib #
+      ##########
+      
       [:rand, [], :int32],
       [:srand, [:uint32], :void],
       [:malloc, [:uint32], :pointer],
@@ -14,20 +80,9 @@ module JIT
       [:free, [:pointer], :void],
       [:abs, [:int32], :int32],
       
-      # stdio
-      [:fopen, [:stringz, :stringz], :pointer],
-      [:fread, [:pointer, :uint64, :uint64, :pointer], :uint64],
-      [:fclose, [:pointer], :int32],
-      [:printf, [:stringz, :varargs], :int32],
-      [:fprintf, [:pointer, :stringz, :varargs], :int32],
-      [:sprintf, [:stringz, :stringz, :varargs], :int32],
-      [:scanf, [:stringz, :varargs], :int32],
-      [:fscanf, [:pointer, :stringz, :varargs], :int32],
-      [:puts, [:stringz], :int32],
-      [:putchar, [:int32], :int32],
-      [:getchar, [], :int32],
-      
-      # time
+      ########
+      # time #
+      ########
       [:time, [:pointer], :int64],
       
     ].each do |name, param_types, return_type|
